3df64bd8d5052cc0e9fe61dd0d0547f883db5bd0
tutinhhao123 <85778070+tutinhhao123@users.noreply.github.com>
2022-06-14 20:50:40 +0700

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,74 +1,159 @@
- <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+     
+ <footer class="nb-footer">
+<div class="container">
+<div class="row">
 
-  <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2020 <a href="https://adminlte.io">Duy Thắng</a>.</strong> All rights
-    reserved.
-  </footer>
 
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+	<h2 class="title">Hỗ trợ khách hàng</h2>
+	<ul class="list-unstyled">
+		<li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Hướng dẫn mua hàng online</a></li></br>
+		Miễn Phí Giao Hàng -Cho Đơn Hàng Trên : 500.000 Vnd</br>
+	</ul>
 </div>
-<!-- ./wrapper -->
+</div>
 
-<!-- jQuery 3 -->
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+	<h2 class="title">Thông Tin Liên Hệ</h2>
+	<ul class="list-unstyled">
+		<li><a href="#" title=""><i class="fa fa-angle-double-right"></i> Thông tin liên hệ</a></li>
+		<li><a href="about.php" title=""><i class="fa fa-angle-double-right"></i> Giới thiệu về chúng tôi</a></li>
+		Địa Chỉ Giao Dịch : Sài Gòn Quận 12 Tp.HCM</br>
+		Fax : +84 0976972771 </br>
+		Email : nh0903991234@gmail.com
+	</ul>
+</div>
+</div>
 
-<script src="js/jquery.min.js"></script>
-<script src="js/jquery-ui.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/adminlte.min.js"></script>
-<script src="js/dashboard.js"></script>
-<script src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
-<script src="tinymce/tinymce.min.js"></script>
-<script src="tinymce/config.js"></script>
-<script src="js/function.js"></script>
-<!-- Latest compiled and minified CSS & JS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"s></script>
-<script src="js/jaxx.js"></script>
-<script>
-  function hien_thi_anh(file){
-    // alert(1234);
-    if (file.files[0].name) {
-      var reader = new FileReader();
-      reader.onload = function(e){
-        $('#show_img').attr('src',e.target.result);
-      }
-      reader.readAsDataURL(file.files[0]);
-    }
-  }
-  function them_anh_khac(){
- var more_img = ' <input type="file" name="image_orther[]" >';
- $('#more_img').append(more_img);
-    
-  }
-</script>
-<script>
-  $(document).ready( function () {
-    $('#myTable').DataTable({
-      "language": {
-            "lengthMenu": "Xem _MENU_ Sản Phẩm",
-            "zeroRecords": "Không tìm thấy dòng nào phù hợp",
-            "info": "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ Sản Phẩm",
-            "infoEmpty": "Đang xem 0 đến 0 trong tổng số 0 mục",
-            "infoFiltered": "(được lọc từ _MAX_ mục)",
-            "sInfoPostFix":  "",
-            "sSearch":       "Tìm Kiếm Sản Phẩm:",
-            "sUrl":          "",
-              "paginate": {
-        // "first":      "Trước ",
-        // "last":       "Sau",
-        "next":       "Tiến",
-        "previous":   "Trước"
-    },
-          
-        }
-       
-      
-    });
-});
-</script>
-   </body>
+<div class="col-md-3 col-sm-6">
+<div class="footer-info-single">
+	<h2 class="title">Phương Phức Thanh Toán</h2>
+	 <a href=""><img src="img/hhhhh.png" alt="shopping image" /></a>
+	
+</div>
+</div>
+</div>
+</div>
+
+<section class="copyright">
+<div class="container">
+<div class="row">
+<div class="col-sm-12">
+<p style="color:#fff">Copyright © <?php echo date('Y') ?> Design : Nguyễn Duy Thắng -Shop Gấu Bông </p>
+</div>
+<div class="col-sm-6"></div>
+</div>
+</div>
+</section>
+</footer>
+</body>
 </html>
+</footer>
+
+        <!-- FOOTER-TOP-AREA END -->
+        <!-- COPYRIGHT-AREA START -->
+      
+        <!-- COPYRIGHT-AREA END -->
+		<!-- JS 
+			===============================================-->
+			  <script src="js/jquery.min.js"></script>
+    <script>
+        jQuery.noConflict();
+    </script>
+    
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/resize.js"></script>
+    <script src="js/off-canvas.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/custom.js"></script>
+			<!-- jquery js -->
+			<script src="js/vendor/jquery-1.11.3.min.js"></script>
+
+			<!-- fancybox js -->
+			<script src="js/jquery.fancybox.js"></script>
+
+			<!-- bxslider js -->
+			<script src="js/jquery.bxslider.min.js"></script>
+
+			<!-- meanmenu js -->
+			<script src="js/jquery.meanmenu.js"></script>
+
+			<!-- owl carousel js -->
+			<script src="js/owl.carousel.min.js"></script>
+<!-- <script src="http://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script> -->
+			<!-- nivo slider js -->
+			<script src="js/jquery.nivo.slider.js"></script>
+
+			<!-- jqueryui js -->
+			<script src="js/jqueryui.js"></script>
+
+			<!-- bootstrap js -->
+			<script src="js/bootstrap.min.js"></script>
+
+			<!-- wow js -->
+			<script src="js/wow.js"></script>		
+			
+		
+			<!-- main js -->
+			<script src="js/main.js"></script>
+			<script type="text/javascript" src="js/jaxgh.js"></script>
+		    <script src="js/jquery.validate.min.js"></script>
+		   <script src="js/check.js"></script>
+		   <script src="js/jquery.flexslider.js"></script>
+		   <script src="js/custom.js"></script>
+		  <script>
+       // jQuery for flexslider------------------
+       jQuery(window).load(function() {
+           // The slider being synced must be initialized first
+           jQuery('#carousel').flexslider({
+               animation: "slide",
+               controlNav: false,
+               animationLoop: false,
+               slideshow: false,
+               directionNav: false,
+               autoHeight: true,
+               itemWidth: 80,
+               itemMargin: 80,
+               asNavFor: '#slider'
+           });
+
+           jQuery('#slider').flexslider({
+               animation: "slide",
+               controlNav: false,
+               animationLoop: false,
+               directionNav: false,
+               autoHeight: true,
+               slideshow: false,
+               sync: "#carousel"
+           });
+
+       });
+   </script>
+	<script>
+        var phone = jQuery('#mainbody_contentbody_reg_phone');
+                phone_pattern = /^(\+84|0)((1([0-9]{9}))|(9([0-9]{8}))|(8([0-9]{8})))$/;
+                if (phone.val() != "" && !phone.val().match(phone_pattern)) {
+                    phone.addClass('validation-failed');
+                    jQuery('#register_error').html('Số điện thoại không hợp lệ.');
+                    phone.focus();
+                    return false;
+                }
+                // if (jQuery('#chkRequirement').is(":checked") == false) {
+                //     jQuery('#register_error').html('Bạn phải đồng ý với điều khoản sử dụng.');
+                //     return false;
+                // }
+                // if (jQuery('#register_error').html() != '') {
+                //     return false;
+                // } else {
+                //     return true;
+                // }
+    </script>
+   
+		  
+		 
+		</body>
+					
+
+		</html>
