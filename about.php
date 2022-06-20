365c392bba395ff3efe70241e894d4f7736ff77c
tutinhhao123 <85778070+tutinhhao123@users.noreply.github.com>
2022-06-20 20:03:52 +0700

    Add files via upload

---
+++ about.php
@@ -0,0 +1,120 @@
+      <?php include('header.php'); ?>
+   
+        <!-- HEADER-MIDDLE END -->
+        <!-- MAIN-MENU-AREA START -->
+              	<!-- MAIN-MENU-AREA END -->
+        	<!-- HEADER-BOTTOM-AREA START -->
+        	  <section class="main-content-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <h2 class="font-update-about">Giới thiệu</h2>
+                    </div>      
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <!-- SINGLE-ABOUT-INFO START -->
+                        <div class="single-about-info">
+                            <div class="our-company">
+                                <h3>Về Chúng Tôi</h3>
+                                <p>Shop Gấu Bông là shop quà tặng Chuyên về các sản phẩm Gấu Bông. Các sản phẩm Gấu Bông , Móc khóa thú nông là hàng Nhập Khẩu được làm với chất liệu cao cấp , nhồi 100% GÒN tinh khiết , an toàn cho người sử dụng , đặc biệt là trẻ em .Shop Gấu Bông  được thành lập từ năm 2015 , với hơn 6 năm hoạt động . Tuy còn non trẻ so với nhiều cửa hàng gấu bông khác nhưng nhờ khai thác thế mạnh vốn có về công nghệ thông tin và đội ngũ nhân viên nhiệt tình, trẻ trung nên đã nhận được sự tín nhiệm của đông đảo khách hàng. Website Gấu Bông Online được thành lập nhằm mục đích mang niềm vui, hạnh phúc và chia sẽ nổi buồn cho mọi người. Mang mọi người xích lại gần nhau hơn, với những chú gấu bông là cầu nối tinh thần giữa con người với nhau.</p>    
+                                <p>Shop Gấu Bông cung cấp đầy đủ các sản phẩm và dịch vụ về gấu bông như gấu bông tặng sinh nhật, gấu bông 20/10, gấu bông 8/3, gấu bông 20/11, gấu bông lễ tình nhân, gấu bông quốc tế thiếu nhi 1/6, gấu bông tặng trung thu, gấu bông chúc mững lễ tốt nghiệp, gấu bông cầu hôn, gấu bông tặng người thân, gấu bông trang trí trong nhà…  Đội ngũ nhân viên nhiệt tình và yêu nghề, với phương châm tất cả vì lợi ích khách hàng. Và mỗi nhân viên tại shop gấu bông , phải hiểu rõ triết lý: liên tục đổi mới, biết lắng nghe ý kiến khách hàng, biết sáng tạo trong công việc – đây cũng chính là nền tảng vững chắc để shop gấu bông chúng tôi phát triển và vững bước trên thị trường thú nhồi bông như hiện nay.</p>
+                            </div>
+                        </div>
+                        <!-- SINGLE-ABOUT-INFO END -->
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="single-about-info">
+                            <!-- OUR-TEAM START -->
+                            <div class="our-team">
+                                <h3>Sản phẩm</h3>
+                                <p>Với sự đa dạng về sản phẩm, mẫu mã, giá cả phù hợp với mọi đối tượng khác nhau, tự tin sẽ đi sâu hơn vào thị trường thú nhồi bông của Việt Nam trong tương lai. Sắp tới Shop Gấu Bông của chúng tôi sẽ cho ra mắt thêm nhiều sản phẩm : gấu teddy cao cấp, gấu teddy giá rẻ, gấu bông hoạt hình,  gấu teddy size to, teddy size nhỏ, mèo bông, heo bông, chuôt, bông, chó bông, cá sấu nhồi bông, khổng long nhồi bông, vịt bông, gà bông, stitch nhồi bông, khỉ bông, rùa bông, chim cánh cụt nhồi bông, bò nhồi bông, cọp bông, sâu bông, voi bông, cừu bông, nhím bông, gấu teddy lông xoắn hoa hồng, gấu teddy size to khổng lồ…</p>
+                            </div>
+                            <!-- OUR-TEAM END -->
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                        <div class="single-about-info">
+                            <!-- OUR-TESTIMONIALS START -->
+                            <div class="our-testimonials">
+                                <h3>Lời kết:</h3>
+                                <!-- SINGLE-TESTIMONIALS START -->
+                                <div class="single-testimonials">
+                                    <div class="testimonials-text">
+                                        <span class="before"></span>
+                                        Shop Gấu Bông chúng tôi kính gửi lời cảm ơn tới tất cả các quý khách hàng đã đang và sẽ sử dụng các sản phẩm và dịch vụ của chúng tôi. Với tiêu chí: “Khách hàng là trung tâm” và “Sự hài lòng của quý khách hàng là thành công của shop”, chúng tôi luôn mong muốn phục vụ quý khách hàng ngày càng tốt hơn.
+                                        <br><br>
+                                        Trân trọng!
+                                        <span class="after"></span>
+                                    </div>
+                                </div>
+                                <!-- SINGLE-TESTIMONIALS END -->
+                            </div>
+                            <!-- OUR-TESTIMONIALS END -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- MAIN-CONTENT-SECTION END -->
+
+        <!-- COMPANY-FACALITY START -->
+        <section class="company-facality">
+            <div class="container">
+                <div class="row">
+                    <div class="company-facality-row">
+                        <!-- SINGLE-FACALITY START -->
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                            <div class="single-facality">
+                                <div class="facality-icon">
+                                    <i class="fa fa-rocket"></i>
+                                </div>
+                                <div class="facality-text">
+                                    <h3 class="facality-heading-text">GIAO HÀNG FREE</h3>
+                                    <span>cho đơn hàng trên 500.000 VNĐ</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- SINGLE-FACALITY END -->
+                        <!-- SINGLE-FACALITY START -->
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                            <div class="single-facality">
+                                <div class="facality-icon">
+                                    <i class="fa fa-umbrella"></i>
+                                </div>
+                                <div class="facality-text">
+                                    <h3 class="facality-heading-text">HỖ TRỢ 24/7</h3>
+                                    <span>Giải đáp mọi thắc mắc của khách hàng</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- SINGLE-FACALITY END -->
+                        <!-- SINGLE-FACALITY START -->                      
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                            <div class="single-facality">
+                                <div class="facality-icon">
+                                    <i class="fa fa-calendar"></i>
+                                </div>
+                                <div class="facality-text">
+                                    <h3 class="facality-heading-text">CẬP NHẬT THƯỜNG XUYÊN</h3>
+                                    <span>Cập nhật những sản phẩm mới nhất </span>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- SINGLE-FACALITY END -->
+                        <!-- SINGLE-FACALITY START -->                      
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                            <div class="single-facality">
+                                <div class="facality-icon">
+                                    <i class="fa fa-refresh"></i>
+                                </div>
+                                <div class="facality-text">
+                                    <h3 class="facality-heading-text">ĐỔI TRẢ TRONG 30 NGÀY</h3>
+                                    <span>Đổi trả sản phẩm nào trong 30 ngày </span>
+                                </div>
+                            </div>
+                        </div>      
+                        <!-- SINGLE-FACALITY END -->                    
+                    </div>
+                </div>
+            </div>
+        </section>
+        <?php include('footer.php'); ?>
---
+++ blog.php
@@ -0,0 +1,86 @@
+<?php include'header.php'; ?>
+<body>
+    <div class="loading-page">
+        <img src="demos/loading.gif" alt="loading">
+    </div>
+    <section class="tzblog-wrap">
+        <div class="container">
+            <h1 class="tz-title-bold-3">Tin Tức</h1>
+            <div class="row gird-margin">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <article class="blog-gird-item blog-item">
+                        <div class="pageblog-thumb">
+                            <img src="img/gau.jpg" alt="Cheese Shop">
+                            <a href="tintuc1.php" class="tzblog-view"><i class="fa fa-search"></i></a>
+                        </div>
+                        <h4><a href="tintuc1.php">Sự tích gấu Tatty Teddy</a></h4>
+                        <p>
+                            Gấu bông Tatty Teddy là một chú gấu đặc biệt, từng bị bỏ rơi, tuyệt vọng nhưng lại gặp ....
+                        </p>
+                        <span class="tzblog-meta">
+                            <em>September 19, 2020</em>
+                        </span>
+                    </article>
+                </div>
+                
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <article class="blog-gird-item blog-item">
+                        <div class="pageblog-thumb">
+                            <img src="img/gau1.png" alt="Vestibulum auctor tortor">
+                            <a href="tintuc2.php" class="tzblog-view"><i class="fa fa-search"></i></a>
+                        </div>
+                        <h4><a href="tintuc2.php">Câu chuyện về gấu Teddy</a></h4>
+                        <p>
+                            Chú gấu Teddy xuất hiện lần đầu tiên cách đây hơn 10 năm, gắn liền với câu chuyện kể...
+                        </p>
+                        <span class="tzblog-meta">
+                            <em>September 19, 2020</em>
+                           
+                        </span>
+                    </article>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <article class="blog-gird-item blog-item">
+                        <div class="pageblog-thumb">
+                            <img src="img/gau2.jpg" alt="Aenean aliquet mi ligula">
+                            <a href="tintuc3.php" class="tzblog-view"><i class="fa fa-search"></i></a>
+                        </div>
+                        <h4><a href="tintuc3.php">Gấu Teddy tượng trưng cho tình yêu</a></h4>
+                        <p>
+                            Bạn đã bao giờ được tặng gấu bông chưa? Chắc hẳn bạn sẽ...
+                        </p>
+                        <span class="tzblog-meta">
+                            <em>September 19, 2020</em>
+                            
+                        </span>
+                    </article>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                    <article class="blog-gird-item blog-item">
+                        <div class="pageblog-thumb">
+                            <img src="img/gau3.jpg" alt="Curabitur pretium tincidunt">
+                            <a href="tintuc4.php" class="tzblog-view"><i class="fa fa-search"></i></a>
+                        </div>
+                        <h4><a href="tintuc4.php">Gấu teddy là người bạn tri kỷ</a></h4>
+                        <p>
+                            Hầu hết cô gái nào cũng sở hữu 1 vài chú gấu bông,...
+                        </p>
+                        <span class="tzblog-meta">
+                            <em>September 19, 2020</em>
+                           
+                        </span>
+                    </article>
+                </div>
+               
+               
+            </div><!--end class row-->
+
+            <div class="tzpagenavi-blog">
+                <a class="active">1</a>
+                <a href="#">2</a>
+                <a href="#">3</a>
+            </div>
+        </div><!--end class container-->
+    </section><!--end class tzblog-wrap-->
+
+   <?php include'footer.php'; ?>
